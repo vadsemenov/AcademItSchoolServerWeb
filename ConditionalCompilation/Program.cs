372a413d1ccdd0e25f7df1be369da71044eb52e6
Vadim Semenov <semenov@bimdance.io>
2024-01-03 22:01:53 +0300

    Add   ConditinalCompilation

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConditionalCompilation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.WriteLine("This run under Debug!");
+#else
+            Console.WriteLine("This run under Release!");
+#endif
+
+            Console.Read();
+        }
+    }
+}
