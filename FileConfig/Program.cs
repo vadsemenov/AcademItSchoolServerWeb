2dee6389958b6ce5dd1d3c5301153a3b4a825ea3
Vadim Semenov <semenov@bimdance.io>
2024-01-03 22:07:05 +0300

    Add FileConfig

---
+++ FileConfig/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConfig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = ConfigurationManager.AppSettings["SiteUrl"];
+
+            Console.WriteLine(url);
+
+            Console.Read();
+        }
+    }
+}
