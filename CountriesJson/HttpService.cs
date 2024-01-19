3f84b78ea65015aa24cd991c0a1222f7f03cd30f
Vadim Semenov <semenov@bimdance.io>
2024-01-19 19:43:41 +0300

    Fix Countries Json

---
+++ CountriesJson/HttpService.cs
@@ -0,0 +1,16 @@
+﻿namespace CountriesJson;
+
+public static class HttpService
+{
+    public static async Task<string> GetCountriesJson(string url)
+    {
+        var httpClient = new HttpClient();
+
+        using Stream stream = await httpClient.GetStreamAsync(url);
+        var reader = new StreamReader(stream);
+
+        var json = await reader.ReadToEndAsync();
+
+        return json;
+    }
+}
--- CountriesJson/Program.cs
+++ CountriesJson/Program.cs
@@ -42,19 +42,4 @@ namespace CountriesJson
             Console.Read();
         }
     }
-
-    public static class HttpService
-    {
-        public static async Task<string> GetCountriesJson(string url)
-        {
-            var httpClient = new HttpClient();
-
-            using Stream stream = await httpClient.GetStreamAsync(url);
-            var reader = new StreamReader(stream);
-
-            var json = await reader.ReadToEndAsync();
-
-            return json;
-        }
-    }
 }
