f1b2defb641b44d07629fc4ade8f5bd24e2fc473
Vadim Semenov <semenov@bimdance.io>
2024-01-20 20:52:04 +0300

    Fix excel task

--- ExcelExport/Program.cs
+++ ExcelExport/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System.Diagnostics;
 using System.IO;
 
-namespace ExcelExport
+namespace ExcelExport;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var persons = PersonExtension.GetRandomPersons();
+        var persons = PersonExtension.GetRandomPersons();
 
-            var excelFile = new FileInfo("Persons.xlsx");
+        var excelFile = new FileInfo("Persons.xlsx");
 
-            var excelService = new EPPlusService(excelFile);
+        var excelService = new EPPlusService(excelFile);
 
-            excelService.AddPersonsToExcelDocument(persons);
-            excelService.SaveDocument();
+        excelService.AddPersonsToExcelDocument(persons);
+        excelService.SaveDocument();
 
-            Process.Start(excelFile.FullName);
-        }
+        Process.Start(excelFile.FullName);
     }
 }
