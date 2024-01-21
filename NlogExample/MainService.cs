1546285a53e3473ea969142850d6a7e428e27186
Vadim Semenov <semenov@bimdance.io>
2024-01-21 12:05:41 +0300

    Add NLog

---
+++ NlogExample/MainService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NlogExample;
+
+public class MainService
+{
+    private readonly ILogger<MainService> _logger;
+
+    public MainService(ILogger<MainService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoAction(string name)
+    {
+        _logger.LogDebug(20, "Doing hard work! {Action}", name);
+
+        try
+        {
+            throw new Exception();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Exception in DoAction method");
+
+        }
+    }
+}
--- NlogExample/Program.cs
+++ NlogExample/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace NlogExample
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace NlogExample
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .Build();
+
+                var services = new ServiceCollection();
+
+                services.AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                });
+
+                services.AddTransient<MainService>();
+
+                using var servicesProvider = services.BuildServiceProvider();
+
+                var runner = servicesProvider.GetRequiredService<MainService>();
+                runner.DoAction("Action1");
+
+                Console.WriteLine("Press ANY key to exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
     }
 }
